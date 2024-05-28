994eaf207e085a235d52bc55d34a6fb92e20b431
Leadafrique-International <154331671+Leadafrique-International@users.noreply.github.com>
2024-05-28 11:54:58 +0000

    Update Platform.tsx

--- components/Platform.tsx
+++ components/Platform.tsx
@@ -6,18 +6,21 @@ const ShuffleHero = () => {
   return (
     <section className="w-full px-8 py-12 grid grid-cols-1 font-font md:grid-cols-2 items-center gap-8 max-w-6xl mx-auto">
       <div>
-        <span className="block mb-4 text-sm md:text-xl text-purple-500 font-medium">
-          Learn on our E-learning Platform
+        <span className="block mb-4 text-sm md:text-xl text-red-500 font-medium">
+          Register for our upcoming summercamp 2024
         </span>
-        <h3 className="text-2xl md:text-[30px] font-semibold">
-          Visit MyLeadershipFarm.com
-        </h3>
+        <h3 className="text-2xl  font-semibold"></h3>
         <p className="text-base md:text-lg text-slate-700 my-4 md:my-6">
-        Leadership Farm is LeadAfrique International’s learning platform to ENABLE, EMPOWER and EMBOLDEN you to live life on your own defined terms of success. 
+          LearningFarm Summercamp is a 1-week self and purpose discovery summer
+          camp for upper primary, junior high, senior high, and university-bound
+          young people between ages 6 -20
         </p>
-        <Link href="https://www.myleadershipfarm.com/" target="_blank">
+        <Link
+          href="https://docs.google.com/forms/d/e/1FAIpQLScNpKt-e2zsRtXIqXZL7UEQCn2qdh6b5lfpqpyhPZbglF6W0A/viewform"
+          target="_blank"
+        >
           <button className="bg-purple-500 text-white font-medium py-2 px-4 rounded transition-all hover:bg-purple-600 active:scale-95">
-            Find a course
+            Register Now
           </button>
         </Link>
       </div>
@@ -46,51 +49,51 @@ const shuffle = (array: (typeof squareData)[0][]) => {
 const squareData = [
   {
     id: 1,
-    src: "https://res.cloudinary.com/dpbuwo8ha/image/upload/v1678626869/G4C_COVER_1_axvy9c.webp",
+    src: "https://res.cloudinary.com/dpbuwo8ha/image/upload/v1678626862/camp_78_ycvkyf.webp",
   },
   {
     id: 2,
-    src: "https://res.cloudinary.com/dpbuwo8ha/image/upload/v1678626869/G4C_COVER_14_plq5o2.webp",
+    src: "https://res.cloudinary.com/dpbuwo8ha/image/upload/v1678626858/camp_2_xhcxth.webp",
   },
   {
     id: 3,
-    src: "https://res.cloudinary.com/dpbuwo8ha/image/upload/v1678626869/G4C_COVER_2_eqjmcm.webp",
+    src: "https://res.cloudinary.com/dpbuwo8ha/image/upload/v1678626863/camp_10_hbp9b6.webp",
   },
   {
     id: 4,
-    src: "https://res.cloudinary.com/dpbuwo8ha/image/upload/v1678626870/G4C_COVER_15_ctnwth.webp",
+    src: "https://res.cloudinary.com/dpbuwo8ha/image/upload/v1678626866/camp_98_k147yb.webp",
   },
   {
     id: 5,
-    src: "https://res.cloudinary.com/dpbuwo8ha/image/upload/v1678626871/G4C_COVER_17_mkhglj.webp",
+    src: "https://res.cloudinary.com/dpbuwo8ha/image/upload/v1678626866/camp_102_maj6p2.webp",
   },
   {
     id: 6,
-    src: "https://res.cloudinary.com/dpbuwo8ha/image/upload/v1678626870/G4C_COVER_16_rqz6j1.webp",
+    src: "https://res.cloudinary.com/dpbuwo8ha/image/upload/v1678626868/front_hunat9.jpg",
   },
   {
     id: 7,
-    src: "https://res.cloudinary.com/dpbuwo8ha/image/upload/v1678626870/G4C_COVER_16_rqz6j1.webp",
+    src: "https://res.cloudinary.com/dpbuwo8ha/image/upload/v1678626871/camp_25_glt9hm.webp",
   },
   {
     id: 8,
-    src: "https://res.cloudinary.com/dpbuwo8ha/image/upload/v1678626871/G4C_COVER_17_mkhglj.webp",
+    src: "https://res.cloudinary.com/dpbuwo8ha/image/upload/v1678626883/camp_51_hff0ex.webp",
   },
   {
     id: 9,
-    src: "https://res.cloudinary.com/dpbuwo8ha/image/upload/v1678626869/G4C_COVER_2_eqjmcm.webp",
+    src: "https://res.cloudinary.com/dpbuwo8ha/image/upload/v1678626883/camp_51_hff0ex.webp",
   },
   {
     id: 10,
-    src: "https://res.cloudinary.com/dpbuwo8ha/image/upload/v1678626869/G4C_COVER_14_plq5o2.webp",
+    src: "https://res.cloudinary.com/dpbuwo8ha/image/upload/v1678626879/camp_41_klrbyi.webp",
   },
   {
     id: 11,
-    src: "https://res.cloudinary.com/dpbuwo8ha/image/upload/v1678626869/G4C_COVER_1_axvy9c.webp",
+    src: "https://res.cloudinary.com/dpbuwo8ha/image/upload/v1678626883/camp_49_qvr6wv.webp",
   },
   {
     id: 12,
-    src: "https://res.cloudinary.com/dpbuwo8ha/image/upload/v1678626869/G4C_COVER_14_plq5o2.webp",
+    src: "https://res.cloudinary.com/dpbuwo8ha/image/upload/v1678626861/camp_68_ghpdym.webp",
   },
 ];
 
